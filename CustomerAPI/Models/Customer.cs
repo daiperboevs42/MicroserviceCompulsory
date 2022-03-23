b96916e17dd9e23e7697b37a87634a43ad4c29f4
Tien0028 <56824916+Tien0028@users.noreply.github.com>
2022-03-23 12:20:38 +0100

    Changes!

---
+++ CustomerAPI/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerAPI.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string BillingAddress { get; set; }
+        public string ShippingAddress { get; set; }
+        public decimal CreditStanding { get; set; }
+    }
+}
